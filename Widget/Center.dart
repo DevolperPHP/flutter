bd4f6e7b24a510651093bab4b86663d8fdd0afc7
DevolperPHP <devolperphp@gmail.com>
2021-12-02 16:51:14 +0300

    Create Center.dart

---
+++ Widget/Center.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Home(),
+      ),
+    );
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "GET IT Laravel",
+          style: TextStyle(fontFamily: "myfont", fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Text("aa"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text("click"),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+}
