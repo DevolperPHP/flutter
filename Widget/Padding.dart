2c34ccb62719f3ad2d5e312c6378cee3820ec8d6
DevolperPHP <devolperphp@gmail.com>
2021-12-02 17:05:58 +0300

    Create Padding.dart

---
+++ Widget/Padding.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Home(),
+      ),
+    );
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "GET IT Laravel",
+          style: TextStyle(fontFamily: "myfont", fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(90),
+        child: Text("Hello"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text("click"),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+}
