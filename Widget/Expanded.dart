2bc0f443eaa25750d4675b7f25bf078770a0f2ee
DevolperPHP <devolperphp@gmail.com>
2021-12-03 01:55:34 +0300

    Create Expanded.dart

---
+++ Widget/Expanded.dart
@@ -0,0 +1,43 @@
+// ignore_for_file: prefer_const_constructors, deprecated_member_use
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Home(),
+      ),
+    );
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "GET IT Laravel",
+          style: TextStyle(fontFamily: "myfont", fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: Row(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(30),
+              color: Colors.cyan,
+              child: Text("1"),
+            ),
+          ),
+          
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text("click"),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+}
