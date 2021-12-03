5df3fabfd5645f7367b964db42a6b6bb840e0945
DevolperPHP <devolperphp@gmail.com>
2021-12-03 14:54:22 +0300

    Create quote.dart

---
+++ Card_Template/quote.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: prefer_initializing_formals
+
+// ignore: empty_constructor_bodies
+class Quote {
+  late String text;
+  late String author;
+
+  Quote({required this.text, required this.author});
+}
