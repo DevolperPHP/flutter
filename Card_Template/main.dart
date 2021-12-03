28f567bd2f705a3ec79bdb5d0877194023e34030
DevolperPHP <devolperphp@gmail.com>
2021-12-03 14:53:57 +0300

    Create main.dart

---
+++ Card_Template/main.dart
@@ -0,0 +1,67 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+// ignore: unused_import
+import 'quote.dart';
+
+void main() => runApp(const MaterialApp(
+      home: QuoteList(),
+    ));
+
+class QuoteList extends StatefulWidget {
+  const QuoteList({Key? key}) : super(key: key);
+
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(author: "Mohammed Majid", text: "any text in the qoute 1"),
+    Quote(author: "Mohammed Majid", text: "any text in the qoute 2"),
+    Quote(author: "Mohammed Majid", text: "any text in the qoute 3"),
+  ];
+
+  Widget quoteTemplate(quote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.grey[600],
+              ),
+            ),
+            SizedBox(height: 6),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.grey[800],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          title: Text("Awesome Qoutes"),
+          backgroundColor: Colors.redAccent,
+        ),
+        body: Column(
+          // ignore: unnecessary_string_interpolations
+          children: quotes.map((quote) => quoteTemplate(quote)).toList(),
+        ));
+  }
+}
