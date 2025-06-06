45c0bce5a4cac18b7141fe08ad8567c6debe8168
DevolperPHP <devolperphp@gmail.com>
2025-06-06 04:59:41 +0300

    Create ListView.dart

---
+++ Widgets/ListView.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Title")),
+        body: ListView(
+          scrollDirection: Axis.horizontal,
+          children: [
+            Container(width: 100, height: 100, color: Colors.amber),
+            Container(width: 100, height: 100, color: Colors.red),
+            Container(width: 100, height: 100, color: Colors.blue),
+            Container(width: 100, height: 100, color: Colors.green),
+            Container(width: 100, height: 100, color: Colors.pink),
+            Container(width: 100, height: 100, color: Colors.amber),
+            Container(width: 100, height: 100, color: Colors.red),
+            Container(width: 100, height: 100, color: Colors.blue),
+            Container(width: 100, height: 100, color: Colors.green),
+            Container(width: 100, height: 100, color: Colors.pink),
+          ],
+        ),
+      ),
+    );
+  }
+}
